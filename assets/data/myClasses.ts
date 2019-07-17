bec9d458e368e3c592dfd206712b25065f89019b
Kato Lefebvre <52496381+lefebvka-sheridan@users.noreply.github.com>
2019-07-17 10:31:08 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,22 @@
+import { ClassInfo } from '../../app/classInfo';
+
+export const MYCLASSES: ClassInfo[] = [
+  {
+    cname: 'SYST24444',
+    room: 'G407',
+    dow: 'Wed',
+    tod: '8-11'
+  },
+  {
+    cname: 'DBAS32100',
+    room: 'J224',
+    dow: 'Thurs',
+    tod: '9-12'
+  },
+  {
+    cname: 'PROG37721',
+    room: 'E204',
+    dow: 'Wed',
+    tod: '12-3'
+  }
+];
